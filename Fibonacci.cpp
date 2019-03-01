d0feb9c45757a26f97ab0cca829f246717e802ec
Nicole <45316185+nicoschmdt@users.noreply.github.com>
2019-03-01 15:09:21 -0300

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -5,11 +5,11 @@ int main(int args, char* argv[]){
 	int B = 1;
 	int temp;
 	int end = atoi(argv[1]);
-	for(int i = 1; i < end; i++){
+	for(int i = 1; i <= end; i++){
+		std::cout << A << std::endl;
 		temp = A;
 		A = B;
 		B = B + temp;
 	}
-	std::cout << B << std::endl;
 	return 0;
 }
